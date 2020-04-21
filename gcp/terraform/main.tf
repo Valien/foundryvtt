5b02addc928b8fc9e1f92b17f6b0e505299cc872
Allen Vailliencourt <allen.vailliencourt@forty8fiftylabs.com>
2020-04-21 14:41:10 -0400

    Initial GCP configurations.

---
+++ gcp/terraform/main.tf
@@ -0,0 +1,206 @@
+#########################################
+# Main Terraform file for GCP Provisioning
+#########################################
+
+# storing terraform state on app.terraform.io
+# terraform {
+#   backend "remote" {
+#     organization = ""
+
+#     workspaces {
+#       name = ""
+#     }
+#   }
+# }
+
+# selecting GCP region
+provider "google" {
+  version = "~> 2.0"
+  region  = var.region
+  project = var.project
+  zone    = var.zone
+}
+
+# setting minimum versions for providers
+provider "random" {
+  version = "~> 2.2"
+}
+
+provider "tls" {
+  version = "~> 2.1"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "external" {
+  version = "~> 1.2"
+}
+
+# creating network
+resource "google_compute_network" "terraform-gcp" {
+  name = "${var.prefix}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "terraform-gcp" {
+  name = "${var.prefix}-subnet"
+  region = var.region
+  network = google_compute_network.terraform-gcp.self_link
+  ip_cidr_range = var.subnet_prefix
+}
+
+resource "google_compute_firewall" "terraform-gcp" {
+  name = "default-allow-ssh-http"
+  network = google_compute_network.terraform-gcp.self_link
+
+# Port 30000 for default Foundry VTT. Adjust as necessary.
+  allow {
+    protocol = "tcp"
+    ports = ["22", "80", "443", "30000"]
+  }
+
+  # @TODO: set for home IP only for SSH
+  # currently allows traffic from everyone to instances with http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["foundry-server"]
+}
+
+# creating key pair for each instance
+# do not use this for prod - https://www.terraform.io/docs/providers/tls/r/private_key.html
+# good for testing only
+resource "tls_private_key" "terraform-gcp" {
+  algorithm = "RSA"
+  rsa_bits = "4096"
+}
+
+# create random ID for GCP instance
+resource "random_id" "app-server-id" {
+  #prefix      = "${var.prefix}-terraform-gcp-"
+  byte_length = 8
+}
+
+resource "google_compute_instance" "terraform-gcp" {
+  name = "${var.instance_name}-terraform-gcp-${random_id.app-server-id.hex}"
+  #zone = var.zone
+  # or
+  #zone = "${var.region}-d"
+  machine_type = var.machine_type
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  # saves some time by running a startup script to patch system and/or install any software sans ansible
+  #metadata_startup_script = "sudo apt-get update"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.terraform-gcp.self_link
+    access_config {
+
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${chomp(tls_private_key.terraform-gcp.public_key_openssh)} terraform"
+  }
+
+  tags = ["foundry-server"]
+}
+
+# set Route 53 DNS
+# resource "aws_route53_record" "terraform-dns" {
+#   zone_id = var.route53_zone_id
+#   name    = var.dns_hostname
+#   type    = "A"
+#   ttl     = "300"
+#   records = ["${aws_eip.terraform-aws.public_ip}"]
+# }
+
+# generate a temp key for this instance
+# do not use this for prod - https://www.terraform.io/docs/providers/tls/r/private_key.html
+# good for testing only
+# resource "tls_private_key" "terraform-aws" {
+#   algorithm = "RSA"
+# }
+
+# creates usable local var
+# https://www.terraform.io/docs/configuration/locals.html
+# locals {
+#   private_key_filename = "${var.prefix}-ssh-key.pem"
+# }
+
+
+# obtain current external IP address for SSH Ingress
+# thanks to this post - https://letslearndevops.com/2018/09/24/getting-own-ip/
+# data "external" "terraform-gcp" {
+#   program = ["bash", "-c", "curl 'https://ipinfo.io/json'"]
+# }
+
+# output "current_public_ip" {
+#   value = data.external.terraform-aws.result.ip
+# }
+
+# checking server status before firing off Ansible playbook.
+# resource "null_resource" "terraform-gcp" {
+#   provisioner "remote-exec" {
+#     inline = ["echo 'The server is up.'"]
+
+#     connection {
+#       type        = "ssh"
+#       user        = var.ssh_user
+#       private_key = tls_private_key.terraform-gcp.private_key_pem
+#       host        = gcp_compute_instance.terraform-gcp.network_interface.0.access_config.0.nat_ip
+#     }
+#   }
+  # requires you to set .ansible.cfg host checking to False.
+  # todo: call ansible playbook that will configure EC2 instance
+
+  # # copying generated key to ~/.ssh folder and changing permissions so the following local-exec can execute.
+  # provisioner "local-exec" {
+  #   command = "echo '${tls_private_key.terraform-aws.private_key_pem}' > ~/.ssh/${local.private_key_filename}" #| chmod 0400 ~/.ssh/${local.private_key_filename}"
+  # }
+
+  # # somehow pipes is causing a file not found issue so splitting up commands
+  # provisioner "local-exec" {
+  #   command = "chmod 0400 ~/.ssh/${local.private_key_filename}"
+  # }
+
+  # running ansible playbook and/or ad-hoc commands
+  # provisioner "local-exec" {
+  #   command = "ansible all -i '${aws_eip.terraform-aws.public_ip},' -m ping -u ${var.ssh_user} --private-key=~/.ssh/${local.private_key_filename}"
+  # }
+
+  # output IP and DNS to ansible hosts file 2 directories up.
+  # provisioner "local-exec" {
+  #   command = "echo ${aws_eip.terraform-aws.public_ip} > ../../ansible/hosts"
+  #   #command = "echo aws_eip.eip.public_dns > ../../ansible/hosts"
+  # }
+
+  # on destroy will also remove the ssh key from ~/.ssh
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = "rm -f ~/.ssh/${local.private_key_filename}"
+#   }
+#  }
+
+# if you need to dump any data to the terminal
+
+# output "ip" {
+#   value = "${aws_eip.ip.public_ip}"
+# }
+
+# output "dns" {
+#   value = "${aws_eip.dns.public_dns}"
+# }
+
+# output "public_ip" {
+#   value = "${aws_instance.ansible-node.public_ip}"
+# }
+
+# output "public_dns" {
+#   value = "${aws_instance.ansible-node.public_dns}"
+# }
---
+++ gcp/terraform/variables.tf
@@ -0,0 +1,69 @@
+#########################################
+# Variables file for GCP Provisioning
+#########################################
+
+variable "prefix" {
+  description = "Prefix for resources"
+  default = "foundry-vtt"
+}
+
+# us-east1 - zone b, c, d
+# us-east4 - zone a, b, c
+variable "region" {
+    #type = list(string)
+    description = "GCP region to launch server"
+    default     = "us-east1"
+}
+
+variable "zone" {
+  description = "GCP Zone to use"
+  default     = "us-east1-d"
+}
+
+# This will prompt you for your GCP project. If you have one to enter you can set the variable so that it always pick this one.
+variable "project" {
+  description = "GCP Project ID"
+  default     = ""
+}
+
+variable "instance_name" {
+    default = "foundry-vtt"
+}
+
+variable "address_space" {
+  description = "Address space for the VPC"
+  default = "10.10.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "Address prefix for subnet."
+  default = "10.10.10.0/24"
+}
+
+# GCP calls them machine types vs AWS instance types
+# https://cloud.google.com/compute/docs/machine-types
+# f1-micro works for GCP free credits. If you need a larger machine type then adjust this.
+variable "machine_type" {
+  default = "f1-micro"
+ }
+
+# https://www.terraform.io/docs/providers/google/r/compute_instance.html#boot_disk
+variable "image" {
+  description = "type of OS image to use"
+  default     = "ubuntu-1910-eoan-v20200413a" # not working for some reason: "ubuntu-os-cloud/ubuntu-1910-lts"
+}
+
+# @TODO:
+# the below zone id is for chumpmonkey.com TLD
+# variable "route53_zone_id" {
+#   description = "Zone ID for f8flabs.com "
+#   default = ""
+# }
+
+ variable "dns_hostname" {
+  default = "foundry.chumpmonkey.com"
+ }
+
+variable "ssh_user" {
+  default = "ubuntu"
+}
